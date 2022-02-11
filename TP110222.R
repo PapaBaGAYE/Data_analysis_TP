4fdfa751499aa264c29ee18297e0675c52f7c353
Papa Ba GAYE <83582380+PapaBaGAYE@users.noreply.github.com>
2022-02-11 12:04:39 +0000

    Add files via upload

---
+++ TP110222.R
@@ -0,0 +1,21 @@
+# data(decathlon2)
+# head(decathlon2)
+data = decathlon2[1:23, 1:10]
+data
+
+pairs(data[, 1:4], pch = 19)
+
+matrice_correlation = cor(data)
+corrplot(matrice_correlation, is.corr = FALSE)
+
+data_scale = as.data.frame(scale(data)) # dataset normalisé
+res.pca <- PCA(data_scale, graph = FALSE)
+print(res.pca)
+
+mean(data_scale$X100m) # moyenne centrée
+sd(data_scale$X100m) # ecart type reduit
+
+eig.val <- get_eigenvalue(res.pca)
+round(eig.val, 2)
+
+fviz_eig(res.pca, addlabels = TRUE, ylim=c(0, 50))
