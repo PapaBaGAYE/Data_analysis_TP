6b7c048bb2143abe79a96080d2ff4b595d38995b
Papa Ba GAYE <83582380+PapaBaGAYE@users.noreply.github.com>
2022-02-15 10:11:49 +0000

    Add files via upload

--- TP110222.R
+++ TP110222.R
@@ -1,5 +1,9 @@
 # data(decathlon2)
 # head(decathlon2)
+
+library(FactoMineR)
+library(factoextra)
+library("corrplot")
 data = decathlon2[1:23, 1:10]
 data
 
@@ -8,11 +12,11 @@ pairs(data[, 1:4], pch = 19)
 matrice_correlation = cor(data)
 corrplot(matrice_correlation, is.corr = FALSE)
 
-data_scale = as.data.frame(scale(data)) # dataset normalisÃ©
+data_scale = as.data.frame(scale(data)) # dataset normalisé
 res.pca <- PCA(data_scale, graph = FALSE)
 print(res.pca)
 
-mean(data_scale$X100m) # moyenne centrÃ©e
+mean(data_scale$X100m) # moyenne centrée
 sd(data_scale$X100m) # ecart type reduit
 
 eig.val <- get_eigenvalue(res.pca)
@@ -20,4 +24,13 @@ round(eig.val, 2)
 
 fviz_eig(res.pca, addlabels = TRUE, ylim=c(0, 50))
 
+var = get_pca_var(res.pca)# Extraction des résultats pour les variables.
+
+ind = get_pca_ind(res.pca)# Extraction des résultats pour les variables.
+
+corrplot(var$cos2, is.corr=TRUE)
+
+fviz_pca_var(res.pca, col.var = "black")
+
+fviz_pca_var(res.pca, col.var = "cos2", gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"), repel = TRUE)
 
