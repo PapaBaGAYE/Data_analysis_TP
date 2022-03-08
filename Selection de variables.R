0d94db9c74dd36be3b43d108eb7e590123af0e0f
Papa Ba GAYE <83582380+PapaBaGAYE@users.noreply.github.com>
2022-03-08 14:31:35 +0000

    Selection de variables

--- Selection de variables.R
+++ Selection de variables.R
@@ -14,13 +14,18 @@ cor_mat = cor(swiss)
 corrplot(cor_mat, is.corr=TRUE)
 
 LRT_dev = function(mod1, mod2){
-  1-pchisq(deviance(mod1)-deviance(mod2), df.residual(mod1)-df.residual(mod2))
+  1 - pchisq(deviance(mod1) - deviance(mod2), df.residual(mod1) - df.residual(mod2))
 }
 model0 = lm(Fertility ~ 1, data = swiss)
-model1 = lm(Fertility ~ 1, Infant.Mortality, data = swiss)
+model1 = lm(Fertility ~ Infant.Mortality, data = swiss)
 LRT_dev(model0, model1)
 
-model2 = lm(Fertility ~ 1, Infant.Mortality + Catholic, data = swiss)
+model2 = lm(Fertility ~ Infant.Mortality + Catholic, data = swiss)
 LRT_dev(model1, model2)
+
+model <- lm(Fertility ~., data = swiss)
+step.model <- stepAIC(model, direction = "both")
+
+summary(step.model)
 
 
